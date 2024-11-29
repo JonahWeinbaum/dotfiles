cee188e070b29da7fd4e298f02d9518cf73e1897
JonahWeinbaum <jonah.r.weinbaum.gr@dartmouth.edu>
2024-11-29 16:16:55 -0500

    .fonts/

--- install.sh
+++ install.sh
@@ -14,12 +14,41 @@ safe_link(){
 SCRIPT_PATH=`realpath $0`
 DOTFILES=`dirname $SCRIPT_PATH`
 
+exclude_list=("install.sh" ".gitignore", ".fonts", ".bashrc", ".zshrc")
+
 # Install other dotfiles
 for f in `ls -A $DOTFILES`; do
     if [[ "install.sh" != "$f" ]] && [[ ".gitignore" != "$f" ]]; then
 	safe_link "$DOTFILES/$f"
     fi
 done
+
+if [[ -e "$HOME/`basename .bashrc`" ]]; then
+    echo "Detected bash terminal"
+    read -p "Are you sure you want to delete '.bashrc'? (y/n): " response
+
+    if [[ "$response" == "y" ]] || [[ "$response" == "Y" ]]; then
+       rm "$HOME/`basename .bashrc`"
+       echo "$HOME/`basename .bashrc` has been deleted."
+    else
+       echo "$HOME/`basename .bashrc` was not deleted."
+    fi
+    safe_link "$DOTFILES/.bashrc"
+fi
+
+if [[ -e "$HOME/`basename .zshrc`" ]]; then
+    echo "Detected bash terminal"
+    read -p "Are you sure you want to delete '.zshrc'? (y/n): " response
+
+    if [[ "$response" == "y" ]] || [[ "$response" == "Y" ]]; then
+       rm "$HOME/`basename .zshrc`"
+       echo "$HOME/`basename .zshrc` has been deleted."
+    else
+       echo "$HOME/`basename .zshrc` was not deleted."
+    fi
+    safe_link "$DOTFILES/.zshrc"
+fi
+
 
 # Set git user
 [ -z `git config --global user.name` ] && git config --global user.name "JonahWeinbaum"
